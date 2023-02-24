95da20106f472156c5850762999fb23962b9b318
Brian Ballantine <bballantine@earnestresearch.com>
2023-02-24 11:15:18 -0500

    moved sailing subproject out

--- sailing/src/sailing.h
+++
@@ -1,6 +0,0 @@
-#ifndef SAILING_H_
-#define SAILING_H_
-
-int sailing_demo(void);
-
-#endif // SAILING_H_
--- sailing/src/test.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TEST_H
-#define __TEST_H
-
-int g_test(int argc, char *argv[]);
-
-#endif
